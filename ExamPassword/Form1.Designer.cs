5b094b16131b1d72ac9c24bff0a9601194587b31
SteecH <v.nikita2003@rambler.ru>
2024-01-12 01:23:33 +0300

    Добавьте файлы проекта.

---
+++ ExamPassword/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ExamPassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.buttonChangeKey = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelMain
+            // 
+            this.panelMain.Location = new System.Drawing.Point(12, 22);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(408, 468);
+            this.panelMain.TabIndex = 0;
+            this.panelMain.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMain_Paint);
+            this.panelMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelMain_MouseDown);
+            this.panelMain.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelMain_MouseMove);
+            this.panelMain.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelMain_MouseUp);
+            // 
+            // buttonChangeKey
+            // 
+            this.buttonChangeKey.Location = new System.Drawing.Point(440, 52);
+            this.buttonChangeKey.Name = "buttonChangeKey";
+            this.buttonChangeKey.Size = new System.Drawing.Size(110, 52);
+            this.buttonChangeKey.TabIndex = 1;
+            this.buttonChangeKey.Text = "Задать новый ключ";
+            this.buttonChangeKey.UseVisualStyleBackColor = true;
+            this.buttonChangeKey.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 561);
+            this.Controls.Add(this.buttonChangeKey);
+            this.Controls.Add(this.panelMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Button buttonChangeKey;
+    }
+}
+
---
+++ ExamPassword/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamPassword
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        
+        List<PointF> coords = new List<PointF>();
+        List<int> passwordKey = new List<int>() { 0, 1, 2, 5, 4, 3};
+        List<PointF> userKey = new List<PointF>();
+        bool keyChangeMode = false;
+        public Form1()
+        {
+            InitializeComponent();
+            panelMain.Invalidate();
+            bmp = new Bitmap(panelMain.ClientSize.Width, panelMain.ClientSize.Height);
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, panelMain, new object[] { true });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            keyChangeMode = true;
+            userKey.Clear();
+        }
+
+        private void DrawPasswordCircle()
+        {
+            coords.Clear();
+            int stepX = panelMain.ClientSize.Width / 4;
+            int stepY = panelMain.ClientSize.Height / 4;
+            Graphics graphics = Graphics.FromImage(bmp);
+            SolidBrush brush = new SolidBrush(Color.Aqua);
+            for(int i = 1; i< 4;i++)
+            {
+                graphics.FillEllipse(brush, 1 * stepX, i * stepY, 30, 30);
+                coords.Add(new PointF(1 * stepX, i * stepY));
+                graphics.FillEllipse(brush, 2 * stepX, i * stepY, 30, 30);
+                coords.Add(new PointF(2 * stepX, i * stepY));
+                graphics.FillEllipse(brush, 3 * stepX, i * stepY, 30, 30);
+                coords.Add(new PointF(3 * stepX, i * stepY));
+            }
+
+        }
+
+        private bool ClickOnCircle(float coordX, float coordY)
+        {
+            float maxX;
+            float maxY;
+            foreach(PointF point in coords) 
+            {
+                maxX = point.X+30;
+                maxY = point.Y+30;
+                if (point.X <= coordX && coordX <= maxX && point.Y <= coordY && coordY <= maxY)
+                {
+                    userKey.Add(point);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool KeyIsCorrect()
+        {
+            if (userKey.Count == 6)
+            {
+                int ind = 0;
+                foreach(PointF point in userKey) 
+                {
+                    if(point != coords[passwordKey[ind]]) return false;
+                    ind++;
+                }
+                return true;
+            }
+            else return false;
+        }
+        private void ChangeKey()
+        {
+            passwordKey.Clear();
+            foreach(PointF point in userKey)
+            {
+                for(int i = 0; i < coords.Count; i++) 
+                {
+                    if(point == coords[i])
+                    {
+                        passwordKey.Add(i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void DrawKeyLine()
+        {
+            Graphics graphics = Graphics.FromImage(bmp);
+            //graphics.DrawLines(Pens.Aqua, userKey.ToArray());
+            Pen pen = new Pen(Color.Aqua, 8);
+            for(int i = 0; i < userKey.Count-1;i++)
+            {
+                graphics.DrawLine(pen, userKey[i].X + 15, userKey[i].Y + 15, userKey[i+1].X + 15, userKey[i+1].Y + 15);
+            }
+        }
+
+        private void panelMain_Paint(object sender, PaintEventArgs e)
+        {
+            bmp = new Bitmap(panelMain.ClientSize.Width, panelMain.ClientSize.Height);
+            DrawPasswordCircle();
+            if(userKey.Count > 1) DrawKeyLine();
+            e.Graphics.DrawImageUnscaled(bmp, Point.Empty);
+        }
+
+        
+
+        private void panelMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(!keyChangeMode && ClickOnCircle(e.X, e.Y)) 
+            {
+                panelMain.Invalidate();
+                if (userKey.Count == 6)
+                {
+                    if(KeyIsCorrect())
+                    {
+                        MessageBox.Show("Ключ верный");
+                        userKey.Clear();
+                        panelMain.Invalidate();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ключ не верный");
+                        userKey.Clear();
+                        panelMain.Invalidate();
+                    }
+                    
+                }
+
+            }
+            else if(keyChangeMode && ClickOnCircle(e.X, e.Y))
+            {
+                panelMain.Invalidate();
+                if (userKey.Count == 6)
+                {
+                    ChangeKey();
+                    MessageBox.Show("Ключ заменен");
+                    keyChangeMode = false;
+                    userKey.Clear();
+                    panelMain.Invalidate();
+                }
+            }
+        }
+
+        private void panelMain_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panelMain_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
